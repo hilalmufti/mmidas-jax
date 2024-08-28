c16e4eb340c6364a1fbd1ff6649e209aa702d8ab
Hilal Mufti <hmufti@cs.washington.edu>
2024-08-27 22:51:24 -0700

    Add jax

--- src/mmidas_jax/mnist.py
+++ src/mmidas_jax/mnist.py
@@ -1,2 +1,4 @@
+import jax
+
 def mnist_fun(x):
     return x + 10
--- src/mmidas_jax/mnist_test.py
+++ src/mmidas_jax/mnist_test.py
@@ -1,8 +1,9 @@
 from expecttest import assert_expected_inline
 
+import jax.numpy as jnp
+
 def inc(x):
     return x + 1
-
 
 def test_answer():
     assert_expected_inline(str(inc(4)), """5""")
@@ -10,3 +11,6 @@ def test_answer():
 def test_split():
     s = 'hello world'
     assert_expected_inline(str(s.split()), """['hello', 'world']""")
+
+def test_jnp():
+    assert_expected_inline(str(jnp.array([1, 2, 3])), """[1 2 3]""")
