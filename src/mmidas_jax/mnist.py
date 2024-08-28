77e1de2e39978508867ba969c281e3a7e3694de3
Hilal Mufti <hmufti@cs.washington.edu>
2024-08-27 23:44:53 -0700

    Add relu and more tests

--- src/mmidas_jax/mnist.py
+++ src/mmidas_jax/mnist.py
@@ -1,3 +1,57 @@
+# %%
+import os
+os.environ["ENABLE_PJRT_COMPATIBILITY"] = str(1)
+
+import math
+
+from jax import grad, jit, vmap, random
+import jax.numpy as jnp
+from jax.scipy.special import logsumexp
+
+SEED = 546
+key = random.key(SEED)
+
+# %%
 def mnist_fun(x):
     print("mnist fun called")
     return x + 2
+
+# %%
+def mk_linear(m, n, key, scale=1e-2):
+    w_key, b_key = random.split(key)
+    return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
+
+mk_linear(4, 6, key)
+
+# %%
+def mk_fc(sizes, key):
+    keys = random.split(key, len(sizes))
+    return [mk_linear(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+mk_fc([2, 2, 3, 4, 5], key)
+
+# %%
+def relu(x):
+    return jnp.maximum(0, x)
+
+relu(jnp.array([1, -2, 3]))
+
+# %%
+def predict(params, image):
+    activations = image
+
+logsumexp(jnp.array([1, 2, 3]))
+
+# %%
+
+sizes = [784, 512, 512, 10]
+lr = 0.01
+epochs = 8
+batch_size = 128
+n_targets = 10
+params = mk_fc(sizes, key)
+
+random_flattened_image = random.normal(random.key(1), (2 * 2,))
+
+jnp.dot(jnp.array([1, 2, 1, 1]),  random_flattened_image)
+
--- tests/test_mnist.py
+++ tests/test_mnist.py
@@ -1,9 +1,20 @@
+import os
+
 from expecttest import assert_expected_inline
 import jax
+from jax import random
 import jax.numpy as jnp
+
+from mmidas_jax.mnist import mk_linear, mk_fc, relu
 
 def inc(x):
     return x + 1
+
+def test_jax_devices():
+    assert_expected_inline(str(jax.devices()), """[METAL(id=0)]""")
+
+def test_pjrt():
+    assert_expected_inline(str(os.environ["ENABLE_PJRT_COMPATIBILITY"]), """1""")
 
 def test_answer():
     assert_expected_inline(str(inc(4)), """5""")
@@ -15,5 +26,39 @@ def test_split():
 def test_jnp():
     assert_expected_inline(str(jnp.array([1, 2, 3])), """[1 2 3]""")
 
-def test_jax_devices():
-    assert_expected_inline(str(jax.devices()), """[METAL(id=0)]""")
+def test_mk_linear0():
+    key = random.key(0)
+    assert_expected_inline(str(mk_linear(3, 3, key)), """\
+(Array([[-0.02610558,  0.00033853,  0.01086333],
+       [-0.01480299,  0.01540325,  0.01062516],
+       [ 0.00541748,  0.00017023,  0.00272268]], dtype=float32), Array([ 0.01137878, -0.01220955, -0.00591536], dtype=float32))""")
+    assert_expected_inline(str(mk_linear(1, 3, key)), """\
+(Array([[ 0.00138932],
+       [ 0.00509335],
+       [-0.00531161]], dtype=float32), Array([ 0.01137878, -0.01220955, -0.00591536], dtype=float32))""")
+    assert_expected_inline(str(mk_linear(3, 1, key)), """(Array([[ 0.00138932,  0.00509335, -0.00531161]], dtype=float32), Array([-0.01251539], dtype=float32))""")
+
+def test_mk_linear546():
+    key = random.key(546)
+    assert_expected_inline(str(mk_linear(3, 3, key)), """\
+(Array([[ 0.00455107, -0.00145467, -0.01521307],
+       [ 0.00368185, -0.00640634, -0.01229068],
+       [ 0.00905397, -0.00399138,  0.00254749]], dtype=float32), Array([0.00789843, 0.00347356, 0.0058376 ], dtype=float32))""")
+    assert_expected_inline(str(mk_linear(1, 3, key)), """\
+(Array([[ 0.00018403],
+       [-0.00152481],
+       [-0.00812268]], dtype=float32), Array([0.00789843, 0.00347356, 0.0058376 ], dtype=float32))""")
+    assert_expected_inline(str(mk_linear(3, 1, key)), """(Array([[ 0.00018403, -0.00152481, -0.00812268]], dtype=float32), Array([-0.00778186], dtype=float32))""")
+
+def test_mk_fc():
+    key = random.key(546)
+    assert_expected_inline(str(mk_fc([4, 2, 2, 1], key)), """\
+[(Array([[ 0.00938669,  0.02961616,  0.00263324, -0.00594663],
+       [ 0.00518566,  0.00423364, -0.00554846, -0.01303717]],      dtype=float32), Array([0.00235619, 0.01016501], dtype=float32)), (Array([[ 0.01997421,  0.00094804],
+       [ 0.00542988, -0.0088178 ]], dtype=float32), Array([0.03150228, 0.0044116 ], dtype=float32)), (Array([[ 0.00888148, -0.00981103]], dtype=float32), Array([0.0124264], dtype=float32))]""")
+
+def test_relu():
+    assert_expected_inline(str(relu(jnp.array([1, -2, 3]))), """[1 0 3]""")
+
+def test_random_img():
+    assert_expected_inline(str(random.normal(random.key(1), (2 * 2,))), """[ 0.17269018 -0.46084192  1.2229712  -0.07101909]""")
